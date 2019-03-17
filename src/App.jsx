42a0b7b387fbb7386e4963d8efa09ff562894a6f
Kiril Stas <vesground@gmail.com>
2019-03-17 22:47:02 +0200

    Added initial app layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 
 import 'src/index.less';
 
+import Form from 'src/components/Form';
+import Table from 'src/components/Table';
+
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>Hi!</div>
-        );
-    }
+  state = {
+    users: [],
+  }
+
+  addUser = (newUser) => {
+    const { users } = this.state;
+
+    this.setState({ users: users.push(newUser) });
+  }
+
+  removeUser = (userId) => {
+    console.log(`i ll remove a user with id ${userId}`);
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <dib className='index'>
+        <Form onAddUser={this.addUser} />
+        <Table users={users} removeUser={this.removeUser} />
+      </dib>
+    );
+  }
 }
